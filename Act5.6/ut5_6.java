d18b85b3ba4a213b1f68e8159a7952f3e8a17ecb
CastroEstudios <93282487+CastroEstudios@users.noreply.github.com>
2022-04-22 19:54:54 +0100

    Add files via upload

---
+++ Act5.6/ut5_6.java
@@ -0,0 +1,12 @@
+
+package ut5_6;
+
+import java.util.ArrayList;
+
+public class ut5_6 {
+    
+    ArrayList<Integer> integerList = new ArrayList<>();
+    ArrayList<String> stringList = new ArrayList<>();
+    ArrayList<Persona> personasList = new ArrayList<Persona>();
+    
+}
